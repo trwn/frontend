0a76e7d68bb63e97ec6da6729732ce393a2c50c0
trwn <trwndoesmc@gmail.com>
2021-09-27 14:31:55 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+PsychPunks Gang
---
+++ src/App.js
@@ -0,0 +1,222 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { connect } from "./redux/blockchain/blockchainActions";
+import { fetchData } from "./redux/data/dataActions";
+import * as s from "./styles/globalStyles";
+import styled from "styled-components";
+import i1 from "./assets/images/preview.gif";
+
+export const StyledButton = styled.button`
+  padding: 10px;
+  border-radius: 50px;
+  border: none;
+  background-color: #ffffff;
+  padding: 10px;
+  font-weight: bold;
+  color: #000000;
+  width: 100px;
+  cursor: pointer;
+  box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  :active {
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+  }
+`;
+
+export const ResponsiveWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: stretched;
+  align-items: stretched;
+  width: 100%;
+  @media (min-width: 1000px) {
+    flex-direction: row;
+  }
+`;
+
+export const StyledImg = styled.img`
+  width: 200px;
+  height: 200px;
+  @media (min-width: 1000px) {
+    width: 350px;
+    height: 350px;
+  }
+  transition: width 0.5s;
+  transition: height 0.5s;
+`;
+
+function App() {
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [feedback, setFeedback] = useState(
+    "🢃🢃 Please input how many PsychPunks 🢃🢃 you would like to mint."
+  );
+  const [claimingNft, setClaimingNft] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const claimNFTs = (_amount) => {
+    if (_amount <= 0) {
+      return;
+    }
+    setFeedback("Minting your PsychPunk...");
+    setClaimingNft(true);
+    blockchain.smartContract.methods
+      .mint(blockchain.account, _amount)
+      .send({
+        from: blockchain.account,
+        value: blockchain.web3.utils.toWei(
+          (0.042 * _amount).toString(),
+          "ether"
+        ),
+      })
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback("Sorry, something went wrong please try again later.");
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        setFeedback(
+          "WOW, you now own a PsychPunk. go visit Opensea.io to view it."
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
+
+  const getData = () => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [blockchain.account]);
+
+  return (
+    <s.Screen style={{ backgroundColor: "var(--blue)" }}>
+      <s.Container flex={1} ai={"center"} style={{ padding: 24 }}>
+        <s.TextTitle
+          style={{ textAlign: "center", fontSize: 28, fontWeight: "bold" }}
+        >
+          Mint a PsychPunk
+        </s.TextTitle>
+        <s.SpacerMedium />
+        <ResponsiveWrapper flex={1} style={{ padding: 24 }}>
+          <s.Container flex={1} jc={"center"} ai={"center"}>
+            <StyledImg alt={"example"} src={i1} />
+            <s.SpacerMedium />
+            <s.TextTitle
+              style={{ textAlign: "center", fontSize: 35, fontWeight: "bold" }}
+            >
+              {data.totalSupply}/10000
+            </s.TextTitle>
+          </s.Container>
+          <s.SpacerMedium />
+          <s.Container
+            flex={1}
+            jc={"center"}
+            ai={"center"}
+            style={{ backgroundColor: "#812f1b", padding: 24 }}
+          >
+            {Number(data.totalSupply) == 10000 ? (
+              <>
+                <s.TextTitle style={{ textAlign: "center" }}>
+                  The sale has ended.
+                </s.TextTitle>
+                <s.SpacerSmall />
+                <s.TextDescription style={{ textAlign: "center" }}>
+                  You can still find PsychPunks on{" "}
+                  <a
+                    target={"_blank"}
+                    href={"https://opensea.io/collection/Psych-Punks"}
+                  >
+                    Opensea.io
+                  </a>
+                </s.TextDescription>
+              </>
+            ) : (
+              <>
+                <s.TextTitle style={{ textAlign: "center" }}>
+                  1 PsychPunk costs 0.042 ETH.
+                </s.TextTitle>
+                <s.SpacerXSmall />
+                <s.TextDescription style={{ textAlign: "center" }}>
+                  Max mint per transaction: 10
+                </s.TextDescription>
+                <s.SpacerSmall />
+                <s.TextDescription style={{ textAlign: "center" }}>
+                  {feedback}
+                </s.TextDescription>
+                <s.SpacerMedium />
+                {blockchain.account === "" ||
+                blockchain.smartContract === null ? (
+                  <s.Container ai={"center"} jc={"center"}>
+                    <s.TextDescription style={{ textAlign: "center" }}>
+                      Connect to the ETH network
+                    </s.TextDescription>
+                    <s.SpacerSmall />
+                    <StyledButton
+                      onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(connect());
+                        getData();
+                      }}
+                    >
+                      CONNECT
+                    </StyledButton>
+                    {blockchain.errorMsg !== "" ? (
+                      <>
+                        <s.SpacerSmall />
+                        <s.TextDescription style={{ textAlign: "center" }}>
+                          {blockchain.errorMsg}
+                        </s.TextDescription>
+                      </>
+                    ) : null}
+                  </s.Container>
+                ) : (
+                  <s.Container ai={"center"} jc={"center"} fd={"row"}>
+                    <StyledButton
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+
+                        // you can access the `inputValue` here
+                        // you might want to do some validation on the input here
+
+                        claimNFTs(inputValue);
+                        getData();
+                      }}
+                    >
+                      {claimingNft ? "BUSY" : "MINT"}
+                    </StyledButton>
+                    <input
+                      style={{ width: "50px" }}
+                      value={inputValue}
+                      onChange={(e) => setInputValue(e.target.value)}
+                    />
+                  </s.Container>
+                )}
+              </>
+            )}
+          </s.Container>
+        </ResponsiveWrapper>
+        <s.SpacerSmall />
+        <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
+          <s.TextDescription style={{ textAlign: "center", fontSize: 9 }}>
+            Please make sure you are connected to the right network (ETH
+            Mainnet) and the correct address. Please note: Once you make the
+            purchase, you cannot undo this action.
+          </s.TextDescription>
+        </s.Container>
+      </s.Container>
+    </s.Screen>
+  );
+}
+
+export default App;
